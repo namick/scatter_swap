f6ade0bd6b451bc881de31e613c56835f7fe7cf5
sue445 <sue445@sue445.net>
2017-01-19 12:10:07 +0900

    Add length arg to ScatterSwap.reverse_hash too
    If integerized hashed integer is passed, can not reversible
    
    e.g)
    
    ```ruby
    ScatterSwap.hash(2970439418180909067, 0, 20)
    => "02792428384994356370"
    
    ScatterSwap.reverse_hash("02792428384994356370", 0)
    => "02970439418180909067"
    
    ScatterSwap.reverse_hash(2792428384994356370, 0)
    => "6564430356352013192"
    ```

--- lib/scatter_swap.rb
+++ lib/scatter_swap.rb
@@ -6,7 +6,7 @@ module ScatterSwap
     Hasher.new(plain_integer, spin, length).hash
   end
 
-  def self.reverse_hash(hashed_integer, spin = 0)
-    Hasher.new(hashed_integer, spin, hashed_integer.to_s.length).reverse_hash
+  def self.reverse_hash(hashed_integer, spin = 0, length = 10)
+    Hasher.new(hashed_integer, spin, length).reverse_hash
   end
 end
